01a2318128054b468786e87c9315204761d43e52
Gualtiero Testa <gualtiero.testa@gmail.com>
2015-01-25 17:21:25 +0100

    First commit

---
+++ src/main/java/it/gualtierotesta/mockitousage/abstractclasses/AbstractCase3.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015 Gualtiero Testa <www.gualtierotesta.it>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.gualtierotesta.mockitousage.abstractclasses;
+
+/**
+ * Case 3: method which access instance fields
+ *
+ * @author Gualtiero Testa <www.gualtierotesta.it>
+ */
+public abstract class AbstractCase3 {
+
+    protected boolean type = false;
+    private int count;
+
+    public abstract int myAbstractMethod();
+
+    public String methodToBeTested() {
+        String res;
+        count += 1;
+        if (count > 10) {
+            res = "Too many times";
+        } else {
+            if (type) {
+                res = "Type is true";
+            } else {
+                res = "Type is false";
+            }
+        }
+        return res;
+    }
+
+    // Only for test
+    void setCount(int pAmount) {
+        this.count = pAmount;
+    }
+
+    public AbstractCase3() {
+        System.out.println("Constructor");
+    }
+
+}
---
+++ src/test/java/it/gualtierotesta/mockitousage/abstractclasses/AbstractCase3Test.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2015 Gualtiero Testa <www.gualtierotesta.it>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.gualtierotesta.mockitousage.abstractclasses;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Case 3 unit test
+ *
+ * @author Gualtiero Testa <www.gualtierotesta.it>
+ */
+public class AbstractCase3Test {
+
+    @Test
+    public void testFalsePathWithCountLessThen10() {
+        // given
+        AbstractCase3 sut = Mockito.mock(AbstractCase3.class);
+        Mockito.doCallRealMethod().when(sut).methodToBeTested();
+        // when
+        String result = sut.methodToBeTested();
+        // then
+        Assert.assertEquals("Type is false", result);
+    }
+
+    @Test
+    public void testTruePathWithCountLessThen10() {
+        // given
+        AbstractCase3 sut = Mockito.mock(AbstractCase3.class);
+        Mockito.doCallRealMethod().when(sut).methodToBeTested();
+        sut.type = true;
+        // when
+        String result = sut.methodToBeTested();
+        // then
+        Assert.assertEquals("Type is true", result);
+    }
+
+    @Test
+    public void testCountGreaterThen10WithSetter() {
+        // given
+        AbstractCase3 sut = Mockito.mock(AbstractCase3.class);
+        Mockito.doCallRealMethod().when(sut).methodToBeTested();
+        Mockito.doCallRealMethod().when(sut).setCount(Mockito.anyInt());
+        sut.setCount(15);
+        // when
+        String result = sut.methodToBeTested();
+        // then
+        Assert.assertEquals("Too many times", result);
+    }
+
+}
